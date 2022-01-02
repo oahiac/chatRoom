41597c88335f2675b9f8abdbacd6b31262b976d6
Bondsam0607 <34954783+Bondsam0607@users.noreply.github.com>
2022-01-02 20:43:31 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # chatRoom
-chat room based on epoll
+
+multi-thread chat room based on epoll and poll
+
+The program contains the server and the client.
+
+## Server
+
+The server is responsible for:
+1. Getting new connections
+2. Receving data from the clients
+3. Distributing the message to other clients
+
+In the main thread, I first use epoll to listen on `listenfd` to accept new connections.
+
+When we got the new connection, add its file descriptor into the kernel event table `epollfd`
+
+Also, use epoll to listen on the readable event `EPOLLIN` and writeable event `EPOLLOUT`
+
+When the server gets the message from the client, it will create a child thread to handle the message:
+1. read the tcp buffer, register `EPOLLOUT` event to the kernel event table.
+2. write data to the write buffers of other connections
+
+When the server gets the `EPOLLOUT` event, which means that the socket is ready to write, it will write the data from the write buffer to the socket to send it to the clients.
+
+## Client
+
+Use poll to listen on the fds. 
+
+The client is responsible for:
+- sending message to the server
+- read server message
+
+When the client detects `POLLIN` event from the stdin, use `splice` to redirect stdin to sockfd and send to the server.
+
+When the client detects `POLLIN` event from the sockfd, read data from the server.
--- server.cpp
+++ server.cpp
@@ -15,7 +15,6 @@
 #include <pthread.h>
 #include <libgen.h>
 
-#include <unordered_map>
 
 #define BUFFER_SIZE 1024
 #define MAX_EVENT_NUMBER 1024
@@ -43,6 +42,7 @@ int setnonblocking(int fd) {
   return old_option;
 }
 
+// add fd to the kernel event table
 void addfd(int epollfd, int fd, bool oneshot) {
   epoll_event event;
   event.data.fd = fd;
@@ -56,12 +56,13 @@ void addfd(int epollfd, int fd, bool oneshot) {
   setnonblocking(fd);
 }
 
+// remove fd from the table
 void delfd(int epollfd, int fd) {
   epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
   data.erase(fd);
 }
 
-
+// modity event of fd
 void modfd(int epollfd, int fd, bool in2out) {
   epoll_event event;
   event.data.fd = fd;
@@ -83,38 +84,47 @@ void reset_oneshot(int epollfd, int fd) {
   epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
 }
 
+// working thread to receive data from the client
 void* recv_data(void* arg) {
-  int connfd = ((fds*)arg)->sockfd;
+  int sockfd = ((fds*)arg)->sockfd;
   int epollfd = ((fds*)arg)->epollfd;
   
   //printf("getting data from %d\n", connfd);
 
-  memset(data[connfd].read_buf, '\0', sizeof(data[connfd].read_buf));
+  memset(data[sockfd].read_buf, '\0', sizeof(data[sockfd].read_buf));
   while (1) {
-    int ret = recv(connfd, data[connfd].read_buf, BUFFER_SIZE-1, 0);
+    int ret = recv(sockfd, data[sockfd].read_buf, BUFFER_SIZE-1, 0);
     if (ret == 0) {
-      close(connfd);
-      printf("%s closed the connection\n", data[connfd].username);
-      data.erase(connfd);
-      delfd(epollfd, connfd);
+      close(sockfd);
+      printf("%s closed the connection\n", data[sockfd].username);
+      data.erase(sockfd);
+      delfd(epollfd, sockfd);
       break;
     }
     else if (ret < 0) {
+      // reach end
       if (errno == EAGAIN) {
-        reset_oneshot(epollfd, connfd);
+        reset_oneshot(epollfd, sockfd);
         break;
       }
     }
     else {
-      char * buf = data[connfd].read_buf;
+      char * buf = data[sockfd].read_buf;
+      // remove the newline symbol from the user input
       buf[strcspn(buf, "\n")] = 0;
 
-      printf("%s : %s\n",data[connfd].username, buf);
+      // add client name to the message
+      char withname[BUFFER_SIZE];
+      memset(withname, '\0', BUFFER_SIZE);
+      strcat(withname, data[sockfd].username);
+      strcat(withname, " : ");
+      strcat(withname, buf);
+      printf("%s\n", withname);
 
       for (auto fd : data) {
-        if (fd.first == connfd) continue;
+        if (fd.first == sockfd) continue;
         modfd(epollfd, fd.first, true);
-        strcat(data[fd.first].write_buf, buf);
+        strcat(data[fd.first].write_buf, withname);
       }
       memset(buf, '\0', BUFFER_SIZE);
     }
@@ -122,48 +132,45 @@ void* recv_data(void* arg) {
 }
 
 void* send_data(void* arg) {
-  int connfd = ((fds*)arg)->sockfd;
+  int sockfd = ((fds*)arg)->sockfd;
   int epollfd = ((fds*)arg)->epollfd;
 
   //printf("sending data '%s' to %d\n", data[connfd].write_buf, connfd);
 
   char send_tmp[BUFFER_SIZE];
   memset(send_tmp, '\0', BUFFER_SIZE);
-  strcat(send_tmp, data[connfd].username);
-  strcat(send_tmp, " : ");
-  strcat(send_tmp, data[connfd].write_buf);
+  strcat(send_tmp, data[sockfd].write_buf);
 
-  int ret = send(connfd, send_tmp, strlen(send_tmp), 0);
+  int ret = send(sockfd, send_tmp, strlen(send_tmp), 0);
   assert(ret != -1);
-  memset(data[connfd].write_buf, '\0', BUFFER_SIZE);
-  modfd(epollfd, connfd, false);
+  memset(data[sockfd].write_buf, '\0', BUFFER_SIZE);
+  modfd(epollfd, sockfd, false);
 }
 
 
 int main(int argc, char* argv[]) {
-  if( argc <= 2 )
-  {
-    printf( "usage: %s ip_address port_number\n", basename( argv[0] ) );
-    return 1;
+  if(argc <= 2) {
+    printf("usage: %s ip_address port_number\n", basename(argv[0]));
+    return EXIT_FAILURE;
   }
   const char* ip = argv[1];
-  int port = atoi( argv[2] );
+  int port = atoi(argv[2]);
 
   int ret = 0;
   struct sockaddr_in address;
-  bzero( &address, sizeof( address ) );
+  bzero(&address, sizeof(address));
   address.sin_family = AF_INET;
-  inet_pton( AF_INET, ip, &address.sin_addr );
-  address.sin_port = htons( port );
+  inet_pton(AF_INET, ip, &address.sin_addr);
+  address.sin_port = htons(port);
 
-  int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
-  assert( listenfd >= 0 );
+  int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+  assert(listenfd >= 0);
 
-  ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
-  assert( ret != -1 );
+  ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+  assert(ret != -1);
 
-  ret = listen( listenfd, 5 );
-  assert( ret != -1 );
+  ret = listen(listenfd, 5);
+  assert(ret != -1);
 
 
   epoll_event events[MAX_EVENT_NUMBER];
@@ -194,6 +201,7 @@ int main(int argc, char* argv[]) {
         addfd(epollfd, connfd, true);
       }
       else if (events[i].events & EPOLLIN) {
+        // readable
         pthread_t thread;
         fds infds;
         infds.epollfd = epollfd;
@@ -201,6 +209,7 @@ int main(int argc, char* argv[]) {
         pthread_create(&thread, NULL, recv_data, (void*)&infds);
       }
       else if (events[i].events & EPOLLOUT) {
+        // writeable
         pthread_t thread;
         fds outfds;
         outfds.epollfd = epollfd;
